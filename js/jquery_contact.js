e10af7717724d5a3c0260e66827f52304e03ad4d
DimGael <dimgael98@gmail.com>
2018-09-12 14:35:13 +0200

    Affichage des contacts

--- js/jquery_contact.js
+++ js/jquery_contact.js
@@ -1,20 +1,42 @@
+var nodeConsole = require('console');
+var myConsole = new nodeConsole.Console(process.stdout, process.stderr);
+
 // ------------------------- AFFICHAGE DES CONTACTS ---------------------------------
 $(document).ready(function() {
-
+    /* ************** FONCTION POUR RESET LES CONTACTS **************************/
     function setListeners() {
+        let body = $('body');
         //Listener sur les boutons supprimer
-        $('body').find('.supprimer').on('click', null, function (event) {
+        body.find('.supprimer').on('click', null, function (event) {
             event.preventDefault();
-            contact_suppr_index = $(this).attr('id');
+            let contact_suppr_index = $(this).attr('id');
             supprimerContact(contact_suppr_index);
-            afficherContacts();
-            setListeners();
+            updateListeContact();
         });
+
         // ---- Affichage des détails du contact
-        $('body').find('.afficher').on('click', null, function (event) {
+        body.find('.afficher').on('click', null, function (event) {
             event.preventDefault();
-            alert("affichage");
-            setListeners();
+            //Récupération de l'index du contact
+            let index = $(this).attr('id').split('_')[1];
+            index = index - 1;
+
+            let contact = listeContact[index];
+
+            let article = $('<article>');
+            article.addClass('media__contact');
+
+            $('<header/>')
+                .append('<h1>Contact ' + (index+1) + ' : ' + contact[0] + ' ' + contact[1]+'</h1>')
+                .append($('<em/>').text('Numéro de téléphone : ' + contact[2]))
+                .appendTo(article);
+
+            $('<p/>')
+                .text('Adresse : ' + contact[3])
+                .appendTo(article);
+
+            $('#result').children('article').remove();
+            article.appendTo('#result');
         });
     }
 
@@ -22,6 +44,7 @@ $(document).ready(function() {
         afficherContacts();
         setListeners();
     }
+    /**************************************************************************/
 
     updateListeContact();
 
@@ -34,10 +57,10 @@ $(document).ready(function() {
         event.preventDefault();
         $(this).hide();
 
-        prenom = $( "input[name='prenom']" ).val();
-        nom = $( "input[name='nom']" ).val();
-        adresse = $( "textarea[name='adresse']" ).val();
-        tel = $( "input[name='tel']" ).val();
+        let prenom = $( "input[name='prenom']" ).val();
+        let nom = $( "input[name='nom']" ).val();
+        let adresse = $( "textarea[name='adresse']" ).val();
+        let tel = $( "input[name='tel']" ).val();
 
         ajouterContact(prenom,  nom, tel, adresse);
 
