119ec92bf3028e97f1ac8f4f16eca4fa5f073b5d
DimGael <dimgael98@gmail.com>
2018-09-12 10:43:03 +0200

    Suppression et début affichage d'un contact

--- js/main.js
+++ js/main.js
@@ -1,9 +1,28 @@
 // ------------------------- AFFICHAGE DES CONTACTS ---------------------------------
-$(document).ready(function(){
+$(document).ready(function() {
     afficherContacts();
 
-    //Listener sur les boutons supprimer
-    $('body').find('.supprimer').on('click', null, function(){
-        alert('Supprimer le contact : ' + $(this).attr('id'))
-    })
+    function setListeners() {
+        //Listener sur les boutons supprimer
+        $('body').find('.supprimer').each(function () {
+            $(this).on('click', null, function (event) {
+                event.preventDefault();
+                contact_suppr_index = $(this).attr('id');
+                supprimerContact(contact_suppr_index);
+                afficherContacts();
+                setListeners();
+            })
+        })
+
+        // ---- Affichage des détails du contact
+        $('body').find('.afficher').each(function () {
+            $(this).on('click', null, function (event) {
+                event.preventDefault();
+                alert("affichage");
+                setListeners();
+            })
+        })
+    }
+
+    setListeners();
 });
