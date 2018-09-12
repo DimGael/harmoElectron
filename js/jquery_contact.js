da5ac55fd95151d04815f977984abc34b33ea8a3
DimGael <dimgael98@gmail.com>
2018-09-12 11:39:05 +0200

    On peut supprimer et ajouter un contact

--- js/jquery_contact.js
+++ js/jquery_contact.js
@@ -1,28 +1,51 @@
 // ------------------------- AFFICHAGE DES CONTACTS ---------------------------------
 $(document).ready(function() {
-    afficherContacts();
 
     function setListeners() {
         //Listener sur les boutons supprimer
-        $('body').find('.supprimer').each(function () {
-            $(this).on('click', null, function (event) {
-                event.preventDefault();
-                contact_suppr_index = $(this).attr('id');
-                supprimerContact(contact_suppr_index);
-                afficherContacts();
-                setListeners();
-            })
-        })
-
+        $('body').find('.supprimer').on('click', null, function (event) {
+            event.preventDefault();
+            contact_suppr_index = $(this).attr('id');
+            supprimerContact(contact_suppr_index);
+            afficherContacts();
+            setListeners();
+        });
         // ---- Affichage des détails du contact
-        $('body').find('.afficher').each(function () {
-            $(this).on('click', null, function (event) {
-                event.preventDefault();
-                alert("affichage");
-                setListeners();
-            })
-        })
+        $('body').find('.afficher').on('click', null, function (event) {
+            event.preventDefault();
+            alert("affichage");
+            setListeners();
+        });
     }
 
-    setListeners();
+    function updateListeContact() {
+        afficherContacts();
+        setListeners();
+    }
+
+    updateListeContact();
+
+    $('#btn_add').on('click', null, function(event){
+        event.preventDefault();
+        $('#form_add').toggle();
+    });
+
+    $('#form_add').on('submit', null, function(event){
+        event.preventDefault();
+        $(this).hide();
+
+        prenom = $( "input[name='prenom']" ).val();
+        nom = $( "input[name='nom']" ).val();
+        adresse = $( "textarea[name='adresse']" ).val();
+        tel = $( "input[name='tel']" ).val();
+
+        ajouterContact(prenom,  nom, tel, adresse);
+
+        updateListeContact();
+
+        // --- reset du formulaire
+        $(':input','#form_add')
+            .not(':submit')
+            .val('');
+    })
 });
--- js/listeContact.js
+++ js/listeContact.js
@@ -1,8 +1,9 @@
+/* *************** POUR UTILISER LA CONSOLE
 var nodeConsole = require('console');
 var myConsole = new nodeConsole.Console(process.stdout, process.stderr);
 myConsole.log('It works !');
-
-/*
+************************************************/
+/**
     Module javascript qui va servir à implémenter une liste de contacts
  */
 
@@ -12,9 +13,7 @@ var listeContact = []; //new array
 // Un contact sera de la forme : ["Prénom", "Nom", "Telephone", "Adresse"]
 //Contacts fictifs
 listeContact.push(["Gael", "DIM", "0687988327", "15 rue de coqui"]);
-listeContact.push(["Jean", "DIM", "0687988327", "15 rue de coqui"]);
-listeContact.push(["Michel", "DIM", "0687988327", "15 rue de coqui"]);
-listeContact.push(["Luna", "DIM", "0687988327", "15 rue de coqui"]);
+listeContact.push(["Jean", "Moulin", "", ""]);
 
 function ajouterContact(prenom, nom, tel, adresse){
     listeContact.push([prenom, nom, tel, adresse])
